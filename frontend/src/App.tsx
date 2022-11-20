e6ec33f5aacb95145cdec73f6f9e13aaf01450bf
Rafaela Cirino <rafaelaborbaf@gmail.com>
2022-11-20 17:26:29 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
-    <h1>Hello world</h1>
+    <>
+      <h1>Hello world</h1>
+      <NotificationButton />
+    </>
   )
 }
 
